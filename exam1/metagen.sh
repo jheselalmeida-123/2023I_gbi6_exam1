508e648d70d2c6f12d7a47b59469eb1c8c7b70c5
jheselalmeida-123 <131234680+jheselalmeida-123@users.noreply.github.com>
2023-05-25 23:46:01 -0500

    fragmentos de interes

--- exam1/metagen.sh
+++ exam1/metagen.sh
@@ -1,1 +1,2 @@
-awk -F'\t' '$2=="species" {count++} END {print "Número de registros hasta el nivel de especies: " count}' ../exam1/data/metagen/infants_metagenome.txt | wc -c
+grep -E "TATA|GAGA|GATA" ../exam1/data/metagen/mygenomemap.sam 
+
--- exam1/mirbase.sh
+++ exam1/mirbase.sh
@@ -1,5 +1,5 @@
- 
-echo "Número de artículos hasta la fecha:" `grep -c "PUBMED" miRNA.dat.gz`
+echo "Número de articulos hasta la fecha:" `grep -c "PUBMED" miRNA.dat.gz`
 echo "Número de estudios en Nature:" `grep -c "Nature" miRNA.dat.gz`
 echo "Número de organismo elegans usado:" `grep -c "elegans" miRNA.dat.gz`
-echo "Número de micro RNA estudiados:" `grep -c "139 BP" miRNA.dat.gz` 
+echo "Número de micro RNA estudiados:" `grep -c "139 BP" miRNA.dat.gz`
+
