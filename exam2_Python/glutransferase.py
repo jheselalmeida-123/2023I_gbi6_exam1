1c956ffa10f5afe5627585585f96b07ce2ca2d1c
jheselalmeida-123 <jhesela.almeida@est.ikiam.edu.ec>
2023-07-01 14:16:44 -0500

    Jhesel Almedia ha actualizado el ejercicio 3

--- exam2_Python/glutransferase.py
+++ exam2_Python/glutransferase.py
@@ -1,15 +1,13 @@
 # Escriba aquí su código para el ejercicio 2
 #import modulo
 def source(data): 
-    contador = 0
-    in_sequence = open(data, "r")
+    seqs = open(data, "r")
     secuencias  = open("data/source.gb", "w")
-    for linea in in_sequence: 
-        Entrez.email = "josue.chango@est.ikiam.edu.ec"
+    for linea in seqs: 
+        Entrez.email = "jhesel.almeida@est.ikiam.edu.ec"
         handle=Entrez.efetch(db="nucleotide" ,id=linea ,rettype="gb", retmode="text")
         data=(handle.read())
         secuencias.write(data) 
-        contador += 1 
         print (contador)
     record_gb = list(SeqIO.parse("data/source.gb", "genbank"))
     source = []
@@ -23,27 +21,22 @@ def source(data):
 
 
 def sequences(data): 
-    contador = 0
-    in_sequence = open(data, "r")
+    seqs = open(data, "r")
     secuencias  = open("data/sequences.gb", "w")
-    for linea in in_sequence: 
-        Entrez.email = "josue.chango@est.ikiam.edu.ec"
+    for linea in seqs: 
+        Entrez.email = "jhesel.almeida@est.ikiam.edu.ec"
         handle=Entrez.efetch(db="nucleotide" ,id=linea ,rettype="gb", retmode="text")
         data=(handle.read())
         secuencias.write(data) 
-        contador += 1 
-        print (contador)
     record_gb = list(SeqIO.parse("data/sequences.gb", "genbank"))
-    peptide = []
     MW = []
     indice_ins = []
     for i in range(len(record_gb)):
         pep = record_gb[i].seq.translate()
-        if pep[0] == "M" and not re.search(re.escape("*"), str(pep[0:-1])):
+        if pep[0] == "M" and not re.search(re.escape("x"), str(pep[0:-1])):
             temp = ProteinAnalysis(pep[0:-1])
             MW.append(temp.molecular_weight())
             indice_ins.append(temp.instability_index())
-            peptide.append(pep)
     df = pd.DataFrame() 
     df ["Peso Molecular"] = MW 
     df ["Indice de inestabilidad"] = indice_ins
